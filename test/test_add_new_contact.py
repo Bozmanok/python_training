1f889d90886432d1f1eca5ce52541c404451b631
Bozmanok <45040163+Bozmanok@users.noreply.github.com>
2019-03-23 19:38:30 +0300

    Выделен класс для управления сессиями

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -12,7 +12,7 @@ def app(request):
 
     
 def test_add_new_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_new_contact(Contact(firstname="First test", middlename="Middle test", lastname="Last test",
                                         path_to_photo="C:\\Users\\Asus\\Desktop\\test_image.jpg",
                                         nickname="Nick test", title="Title test", company="Company test",
@@ -21,14 +21,14 @@ def test_add_new_contact(app):
                                         email3="test3@test.tu", homepage="www.test.tu", bday="15", bmonth="January",
                                         byear="1990", aday="15", amonth="January", ayear="1990", new_group="[none]",
                                         address2="Address test", phone2="34567", notes="Notes test"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_new_empty_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_new_contact(Contact(firstname="", middlename="", lastname="", path_to_photo="", nickname="",
                                         title="", company="", address="", home="", mobile="", work="", fax="",
                                         email="", email2="", email3="", homepage="", bday="", bmonth="-", byear="",
                                         aday="", amonth="-", ayear="", new_group="", address2="", phone2="",
                                         notes=""))
-    app.logout()
+    app.session.logout()
