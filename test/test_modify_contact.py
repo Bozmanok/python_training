3d12d83707ad64817d30e667b61048c8f168e72c
Bozmanok <45040163+Bozmanok@users.noreply.github.com>
2019-03-30 21:13:01 +0300

    Добавлены предусловия выполнения тестов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,9 +2,13 @@ from model.contact import Contact
 
 
 def test_modify_first_contact_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="First test"))
     app.contact.modify_first_contact(Contact(firstname="First new name", lastname="Last new name"))
 
 
 def test_modify_first_contact_dates(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="First test"))
     app.contact.modify_first_contact(Contact(bday="30", bmonth="May", byear="2005",
                                              aday="14", amonth="June", ayear="1980"))
