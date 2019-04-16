5e6062e5491ab1369e39af36f474af17deaa33c6
Bozmanok <45040163+Bozmanok@users.noreply.github.com>
2019-04-17 01:40:36 +0300

    Добавлен тест на проверку соответствия данных на главной странице и в форме редактирования у случайного контакта

--- model/contact.py
+++ model/contact.py
@@ -7,7 +7,7 @@ class Contact:
                  company=None, address=None, homephone=None, mobilephone=None, workphone=None, fax=None, email=None,
                  email2=None, email3=None, homepage=None, bday=None, bmonth=None, byear=None, aday=None, amonth=None,
                  ayear=None, new_group=None, address2=None, secondaryphone=None, notes=None, id=None,
-                 all_phones_from_home_page=None):
+                 all_phones_from_home_page=None, all_emails_from_home_page=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -36,9 +36,10 @@ class Contact:
         self.notes = notes
         self.id = id
         self.all_phones_from_home_page = all_phones_from_home_page
+        self.all_emails_from_home_page = all_emails_from_home_page
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s:%s" % (self.id, self.lastname, self.firstname)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and \
--- test/test_phones.py
+++ test/test_phones.py
@@ -1,3 +1,5 @@
+from model.contact import Contact
+from random import randrange
 import re
 
 
@@ -5,6 +7,19 @@ def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+
+
+def test_names_phones_emails_some_contact_on_home_page(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="First test"))
+    contacts = app.contact.get_contact_list()
+    index = randrange(len(contacts))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
 
 
 def test_phones_on_contact_view_page(app):
@@ -26,3 +41,10 @@ def merge_phones_like_on_home_page(contact):
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
                                     [contact.homephone, contact.mobilephone, contact.workphone, contact.secondaryphone]))))
+
+
+def merge_emails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.email, contact.email2, contact.email3]))))
