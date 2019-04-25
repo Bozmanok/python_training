ffdfbc8437a094813bc2fbde4bb6cd7495b16f4a
Bozmanok <45040163+Bozmanok@users.noreply.github.com>
2019-04-25 21:17:13 +0300

    Добавлена параметризация тестов путём "внедрения" этих тестовых данных в фикстуру

---
+++ data/contacts.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="first1", lastname="last1"),
+    Contact(firstname="first2", lastname="last2")
+]
