908bbe59bb6b860fa2eadc28dff9490219e0e2ff
Bozmanok <45040163+Bozmanok@users.noreply.github.com>
2019-04-25 20:37:16 +0300

    Реализовано отделение тестовых данных от сценария по добавлению контакта и добавлена библиотека jsonpickle для сохранения структуры объектов в формат JSON

---
+++ generator/contact.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+import random
+import string
+import os.path
+import jsonpickle
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", middlename="", lastname="", path_to_photo=None, nickname="",
+                               title="", company="", address="", homephone="", mobilephone="", workphone="", fax="",
+                               email="", email2="", email3="", homepage="", bday="", bmonth="-", byear="",
+                               aday="", amonth="-", ayear="", new_group=None, address2="", secondaryphone="",
+                               notes="")] + [
+    Contact(firstname=random_string("firstname", 20), middlename=random_string("middlename", 20),
+            lastname=random_string("lastname", 20), path_to_photo=None, nickname=random_string("nickname", 20),
+            title=random_string("title", 10), company=random_string("company", 10), address=random_string("address", 10),
+            homephone=random_string("homephone", 10), mobilephone=random_string("mobilephone", 10),
+            workphone=random_string("workphone", 10), fax=random_string("fax", 10), email=random_string("email", 10),
+            email2=random_string("email2", 10), email3=random_string("email3", 10), homepage=random_string("homepage", 10),
+            bday="15", bmonth="January", byear=random_string("byear", 5), aday="30", amonth="September",
+            ayear=random_string("ayear", 5), new_group=None, address2=random_string("address2", 20),
+            secondaryphone=random_string("secondaryphone", 10), notes=random_string("notes", 10))
+    for i in range(3)
+]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/contacts.json")
+
+with open(file, "w") as f:
+    jsonpickle.set_encoder_options("json", indent=2)
+    f.write(jsonpickle.encode(testdata))
